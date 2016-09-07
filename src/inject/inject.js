7a4e4ab2eb8cbf5a5e5a6257b191a2208077ca60
peterthomasross <peter.ross@live.com>
2016-09-07 09:27:09 +0100

    inital commit

---
+++ src/inject/inject.js
@@ -0,0 +1,24 @@
+chrome.extension.sendMessage({}, function(response) {
+	var readyStateCheckInterval = setInterval(function() {
+	if (document.readyState === "complete") {
+		clearInterval(readyStateCheckInterval);
+
+		// ----------------------------------------------------------
+		// This part of the script triggers when page is done loading
+		// ----------------------------------------------------------
+		var issueTitle = document.querySelector('.js-issue-title');
+		console.log (issueTitle);
+		re=/id:([^\s]+)/;
+		var check=issueTitle.innerHTML.match(re);
+		console.log (check);
+		if (check.length > 1) {
+			var a = document.createElement('a');
+			a.setAttribute('href', 'https://redriversoftware.tpondemand.com/entity/' + check[1])
+			a.innerHTML = issueTitle.innerHTML;
+			issueTitle.parentNode.replaceChild(a, issueTitle)
+		}
+	}
+}, 100);
+});
+
+// (id:)([^\s]+)
